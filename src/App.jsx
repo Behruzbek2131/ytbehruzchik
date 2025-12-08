1b72e1555f25f1873e31b1da534e7b49d35f5543
Behruzbek2131 <sofarboyevbehruzbek@gmail.com>
2025-12-08 19:19:30 +0500

    o'zgardi

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import "./App.css"
+
+export default function App() {
+  return (
+    <div className="yt_behruzchik">
+      <div className="behruzchik">
+        <div className="behruzchik_info">
+          <img src="/image.png" alt="img" />
+          <h2>YT • Behruzchik</h2>
+        </div>
+        <div className="linklar">
+          <a href="https://t.me/Behruzchik_im" target="_blank" className="link">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/5c/Telegram_Messenger.png" alt="tg call" />
+            <h2>Telegram aloqa!</h2>
+          </a>
+          <a href="https://t.me/Behruz_music_official" target="_blank" className="link">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/5c/Telegram_Messenger.png" alt="tg channel" />
+            <h2>Telegram kanalim!</h2>
+          </a>
+          <a href="https://www.instagram.com/behruzchik_im" target="_blank" className="link">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/f/f2/Instagram-Logo-Round-Color.png" alt="insta" />
+            <h2>Instagram saxifam!</h2>
+          </a>
+          <a href="mailto:eldarqaldiqaraev75@gmail.com?subject=Salom" target="_blank" className="link">
+            <img src="https://static.vecteezy.com/system/resources/previews/042/165/820/non_2x/gmail-icon-transparent-free-png.png" alt="gmail" />
+            <h2>Email aloqa!</h2>
+          </a>
+          <a href="https://www.tiktok.com/@Behruz_oke5" target="_blank" className="link">
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/018/930/463/small/tiktok-logo-tikok-icon-transparent-tikok-app-logo-free-png.png" alt="tiktok" />
+            <h2>TikTok saxifam!</h2>
+          </a>
+          <a href="https://www.facebook.com/ytbehruzchik" target="_blank" className="link">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/2048px-Facebook_f_logo_%282019%29.svg.png" alt="facebook" />
+            <h2>Facebook saxifam!</h2>
+          </a>
+          <a href="https://portfolio-nine-rosy-53.vercel.app/" target="_blank" className="link1">
+            <h2>O'zim Haqida</h2>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
